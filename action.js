b77e3d7b827cce0c0711338f8f1c4fb8c608c3cc
Prakhar Sahay <prakhar.studios@gmail.com>
2020-03-24 22:25:29 -0400

    Begin Re-Writing JS
    Merge main.js into startup.js, move location configs into separate
    file, and start picking lint (variable names should not be a letter
    long, spaces around operators, JSON formatting, etc.).

--- action.js
+++ action.js
@@ -1,76 +1,13 @@
 /**
-n cycles through movers[],handle depletes jail[] or calls on rollMove
+n cycles through players[],handle depletes jail[] or calls on rollMove
 rollMove may call action,action/react will call rollMove
 
 Display ids are bal/loc/ploc
 */
 
-var movers=[{name:"Prateek",bal:1500,locnum:0,injail:0,num:0},
-        {name:"Prakhar",bal:1500,locnum:0,injail:0,num:1},
-        {name:"Willy",bal:1500,locnum:0,injail:0,num:2},
-        {name:"Troy",bal:1500,locnum:0,injail:0,num:3},
-        {name:"Poop",bal:1500,locnum:0,injail:0,num:4}
-];
-var len=movers.length;
-var mess,ram,exmess;
-$(document).ready(function(){
-    mess=document.getElementById("mess");
-    ram=document.getElementById("ram");
-    exmess=document.getElementById("exmess");
-    for(var loop1=0;loop1<len;loop1++){
-        $("#head"+loop1).html(movers[loop1].name+": <span id='loc"+loop1+"'>Go</span><div style='float:right' id='bal"+loop1+"'>$1500</div>");
-    }
-});
-var n=Math.ceil(Math.random()*len)-1;
-var tax=0;
-
-//            ,,,"rgb(219,8,40)","rgb(219,8,40)","","rgb(235,236,54)","rgb(235,236,54)"," ","rgb(235,236,54)",
-//            "rgb(213,232,212)","green","green","#48C","green","","#A6A","rgb(63,101,172)","","rgb(63,101,172)"];
-
-var cols=["rgb(129,77,38)","rgb(153,210,240)","rgb(211,40,132)","orange","rgb(219,8,40)","rgb(235,236,54)","green","rgb(63,101,172)"];
-//              brown           sky blue            pink        orange          red             yellow      green       deep blue
-
-var places=new Array(40);
-places[0]={name:"Go",p:0,col:"rgb(213,232,212)"};
-places[1]={name:"Mediterranean Avenue",p:60,re0:2,re1:10,re2:30,re3:90,re4:160,re5:250,ho:50,own:-1,col:cols[0]};
-places[2]={name:"Community Chest",p:0,col:"#48C"};
-places[3]={name:"Baltic Avenue",p:60,re0:4,re1:20,re2:60,re3:180,re4:320,re5:450,ho:50,own:-1,col:cols[0]};
-places[4]={name:"Income Tax",p:0,col:""};
-    places[5]={name:"Reading Railroad",p:200,re0:25,re1:50,re2:100,re3:200,own:-1,col:""};
-places[6]={name:"Oriental Avenue",p:100,re0:6,re1:30,re2:90,re3:270,re4:400,re5:550,ho:50,own:-1,col:cols[1]};
-places[7]={name:"Chance",p:0,col:"#A6A"};
-places[8]={name:"Vermont Avenue",p:100,re0:6,re1:30,re2:90,re3:270,re4:400,re5:550,ho:50,own:-1,col:cols[1]};
-places[9]={name:"Connecticut Avenue",p:120,re0:8,re1:40,re2:100,re3:300,re4:450,re5:600,ho:50,own:-1,col:cols[1]};
-    places[10]={name:"Just Visiting",p:0,col:"rgb(213,232,212)"};
-places[11]={name:"St. Charles Place",p:140,re0:10,re1:50,re2:150,re3:450,re4:625,re5:750,ho:100,own:-1,col:cols[2]};
-places[12]={name:"Electric Company",p:150,re0:0,re1:0,re2:0,re3:0,re4:0,re5:0,ho:0,own:-1,col:""};
-places[13]={name:"States Avenue",p:140,re0:10,re1:50,re2:150,re3:450,re4:625,re5:750,ho:100,own:-1,col:cols[2]};
-places[14]={name:"Virginia Avenue",p:160,re0:12,re1:60,re2:180,re3:500,re4:700,re5:900,ho:100,own:-1,col:cols[2]};
-    places[15]={name:"Pennsylvania Railroad",p:200,re0:25,re1:50,re2:100,re3:200,own:-1,col:""};
-places[16]={name:"St. James Place",p:180,re0:14,re1:70,re2:200,re3:550,re4:750,re5:950,ho:100,own:-1,col:cols[3]};
-places[17]={name:"Community Chest",p:0,col:"#48C"};
-places[18]={name:"Tennessee Avenue",p:180,re0:14,re1:70,re2:200,re3:550,re4:750,re5:950,ho:100,own:-1,col:cols[3]};
-places[19]={name:"New York Avenue",p:200,re0:16,re1:80,re2:220,re3:600,re4:800,re5:1000,ho:100,own:-1,col:cols[3]};
-    places[20]={name:"Free Parking",p:0,col:"rgb(213,232,212)"};
-places[21]={name:"Kentucky Avenue",p:220,re0:18,re1:90,re2:250,re3:700,re4:875,re5:1050,ho:150,own:-1,col:cols[4]};
-places[22]={name:"Chance",p:0,col:"#A6A"};
-places[23]={name:"Indiana Avenue",p:220,re0:18,re1:90,re2:250,re3:700,re4:875,re5:1050,ho:150,own:-1,col:cols[4]};
-places[24]={name:"Illinois Avenue",p:240,re0:20,re1:100,re2:300,re3:750,re4:925,re5:1100,ho:150,own:-1,col:cols[4]};
-    places[25]={name:"B. & O. Railroad",p:200,re0:25,re1:50,re2:100,re3:200,own:-1,col:""};
-places[26]={name:"Atlantic Avenue",p:260,re0:22,re1:110,re2:330,re3:800,re4:975,re5:1150,ho:150,own:-1,col:cols[5]};
-places[27]={name:"Ventnor Avenue",p:260,re0:22,re1:110,re2:330,re3:800,re4:975,re5:1150,ho:150,own:-1,col:cols[5]};
-places[28]={name:"Water Works",p:150,re0:0,re1:0,re2:0,re3:0,re4:0,re5:0,ho:0,own:-1,col:" "};
-places[29]={name:"Marvin Gardens",p:280,re0:24,re1:120,re2:360,re3:850,re4:1025,re5:1200,ho:150,own:-1,col:cols[5]};
-    places[30]={name:"Go To Jail",p:0,col:"rgb(213,232,212)"};
-places[31]={name:"Pacific Avenue",p:300,re0:26,re1:130,re2:390,re3:900,re4:1100,re5:1275,ho:200,own:-1,col:cols[6]};
-places[32]={name:"North Carolina Avenue",p:300,re0:26,re1:130,re2:390,re3:900,re4:1100,re5:1275,ho:200,own:-1,col:cols[6]};
-places[33]={name:"Community Chest",p:0,col:"#48C"};
-places[34]={name:"Pennsylvania Avenue",p:320,re0:28,re1:150,re2:450,re3:1000,re4:1200,re5:1400,ho:200,own:-1,col:cols[6]};
-    places[35]={name:"Short Line",p:200,re0:25,re1:50,re2:100,re3:200,own:-1,col:""};
-places[36]={name:"Chance",p:0,col:"#A6A"};
-places[37]={name:"Park Place",p:350,re0:35,re1:175,re2:500,re3:1100,re4:1300,re5:1500,ho:200,own:-1,col:cols[7]};
-places[38]={name:"Luxury Tax",p:0,col:""};
-places[39]={name:"Boardwalk",p:700000000000,re0:50,re1:200,re2:600,re3:1400,re4:1700,re5:2000,ho:200,own:-1,col:cols[7]};
+const mess = document.getElementById("mess");
+const ram = document.getElementById("ram");
+const exmess = document.getElementById("exmess");
 
 
 function showCard(locnum) {
@@ -130,18 +67,36 @@ $("#propname").css("backgroundColor",places[locnum].col);
 $("#propname").css("display","block");
 }
 
-function upB(){$("#bal"+mover.num).text("$"+mover.bal)}
-function upL(){$("#loc"+mover.num).text(places[mover.locnum].name);
+function upB() {
+    // Update the view with the current user's balance.
+    $("#bal" + mover.num).text("$" + mover.bal);
+}
+
+function upL() {
+    // Update the view with the current user's location.
+    $("#loc" + mover.num).text(places[mover.locnum].name);
     //$("#board div:eq("+(mover.locnum+1)+")").append($("#marker"+mover.num));
-    if(places[mover.locnum].ho){$("#board").children().eq(mover.locnum).children().first().append($("#marker"+mover.num));}
-    else{$("#board").children().eq(mover.locnum).append($("#marker"+mover.num));}
+    if (places[mover.locnum].ho) {
+        $("#board").children().eq(mover.locnum).children().first().append($("#marker" + mover.num));
+    } else{
+        $("#board").children().eq(mover.locnum).append($("#marker" + mover.num));
     }
-function rand(){return Math.ceil(6*Math.random())}
-var roll1;var roll2;var rollnum;
+}
+
+function rand() {
+    return Math.ceil(6 * Math.random());
+}
+
+var roll1;
+var roll2;
+var rollnum;
 var mover;
 
 function handle(){
-    mover=movers[n];n++;n%=len;
+    mover = players[n];
+    n ++;
+    n %= players.length;
+    
     $("#turn").text(mover.name);
     $("#exmess").append($("#ram"));
     mess.textContent="";
@@ -169,8 +124,6 @@ function goAgain(){
     else if(mover.injail>0){$("#mess").append($("#ram"));return false}
     return true;
 }
-
-
 
 function rollMove() {
     roll1=rand();roll2=rand();
@@ -221,7 +174,7 @@ function action() {//properties or non-properties
             waitForReact=true;
         }
         else if (place.own!=mover.num) {//rent for prop,train,or utility
-            var guy=movers[place.own];
+            var guy=players[place.own];
             var rent;
             switch(mover.locnum){
                 case 12:case 28:rent=4*(roll1+roll2);break;
@@ -259,13 +212,13 @@ function chance(mover) {
     case 7:mess.textContent+="GET OUT OF JAIL FREE. This card may be kept until needed or traded.";break;
     case 8:mess.textContent+="Bank pays you dividend of $50.";mover.bal+=50;break;
     case 9:mess.textContent+='Advance to Illinois Avenue. If you pass "Go" collect $200.';if (mover.locnum>24) {mover.bal+=200}mover.locnum=24;action();break;
-    case 10:mess.textContent+="You have been elected chairman of the board. Pay each player $50.";for(var c=0;c<len;c++){movers[c].bal+=50}mover.bal-=50*len;break;
+    case 10:mess.textContent+="You have been elected chairman of the board. Pay each player $50.";for(var c=0;c<players.length;c++){players[c].bal+=50}mover.bal-=50*players.length;break;
     case 11:mess.textContent+='Go to jail. Go directly to jail, do not pass "Go", do not collect $200.';goToJail();break;
     case 12:mess.textContent+="Advance to the nearest utility. If Unowned, you may buy it from the bank. If Owned, pay owner a total ten times amount thrown on dice.";
             if(mover.locnum>12 && mover.locnum<28){mover.locnum=28}else{mover.locnum=12}
             if(places[mover.locnum].own==-1){action()}
             else if(places[mover.locnum].own!=mover.num){
-                pay(movers[places[mover.locnum].own],10*(roll1+roll2));
+                pay(players[places[mover.locnum].own],10*(roll1+roll2));
             }
             newl=false;
     break;
@@ -275,7 +228,7 @@ function chance(mover) {
             if(places[mover.locnum].own==-1){action()}
             else if(places[mover.locnum].own!=mover.num){
                 var place=places[mover.locnum];
-                pay(movers[place.own],2*place.re0);
+                pay(players[place.own],2*place.re0);
             }
             newl=false;
     break;
@@ -293,7 +246,7 @@ function comChest(mover) {
     case 2: mess.textContent+="Holiday fund matures. Receive $100.";mover.bal+=100;break;
     case 3: mess.textContent+="Bank error in your favor. Collect $200.";mover.bal+=200;break;
     case 4: mess.textContent+='Go to jail. Go directly to jail, do not pass "Go", do not collect $200.';goToJail();break;
-    case 5: mess.textContent+="It is your birthday. Collect $10 from every player.";mover.bal+=10*len;for(var c=0;c<len;c++){movers[c].bal-=10;$("bal"+c).text("$"+movers[c].bal)}break;
+    case 5: mess.textContent+="It is your birthday. Collect $10 from every player.";mover.bal+=10*players.length;for(var c=0;c<players.length;c++){players[c].bal-=10;$("bal"+c).text("$"+players[c].bal)}break;
     case 6: mess.textContent+="Doctor's fees. Pay $50.";mover.bal-=50;break;
     case 7: mess.textContent+="Pay hospital fees of $100.";mover.bal-=100;break;
     case 8: mess.textContent+="You inherit $100.";mover.bal+=100;break;
@@ -330,20 +283,20 @@ function transaction() {
     var giver=prompt("Enter the sender.");
     
     switch(recip){
-        case movers[0].name:movers[0].bal+=parseInt(amount,10);break;
-        case movers[1].name:movers[1].bal+=parseInt(amount,10);break;
-        case movers[2].name:movers[2].bal+=parseInt(amount,10);break;
+        case players[0].name:players[0].bal+=parseInt(amount,10);break;
+        case players[1].name:players[1].bal+=parseInt(amount,10);break;
+        case players[2].name:players[2].bal+=parseInt(amount,10);break;
         default:mess.textContent+="Transaction error! Invalid recipient!";giver="anything invalid";
     }
     
     switch(giver){
-        case movers[0].name:movers[0].bal-=amount;break;
-        case movers[1].name:movers[1].bal-=amount;break;
-        case movers[2].name:movers[2].bal-=amount;break;
+        case players[0].name:players[0].bal-=amount;break;
+        case players[1].name:players[1].bal-=amount;break;
+        case players[2].name:players[2].bal-=amount;break;
         case "anything invalid":break;
         default:mess.textContent+="Transaction error! Invalid sender!";
     }
-    $("#bal0").text("$"+movers[0].bal);
-    $("#bal1").text("$"+movers[1].bal);
-    $("#bal2").text("$"+movers[2].bal);
+    $("#bal0").text("$"+players[0].bal);
+    $("#bal1").text("$"+players[1].bal);
+    $("#bal2").text("$"+players[2].bal);
 }
--- startup.js
+++ startup.js
@@ -1,83 +1,138 @@
-var heads,board;
-$(document).ready(function(){
-heads=document.getElementById("heads");
-board=document.getElementById("board");
+// Players are hard-coded for now.
+const Sprite = {
+    BLAZIKEN: 257,
+    SCEPTILE: 254,
+    SWAMPERT: 260,
+    VENUSAUR: 3,
+    CHARIZARD: 6
+}
+
+const players = [
+    {name: "Prateek", bal: 1500, locnum: 0, injail: 0, num: 0, color: "#E80", spriteId: Sprite.BLAZIKEN},
+    {name: "Prakhar", bal: 1500, locnum: 0, injail: 0, num: 1, color: "#0C0", spriteId: Sprite.SCEPTILE},
+    {name: "Willy", bal: 1500, locnum: 0, injail: 0, num: 2, color: "#08F", spriteId: Sprite.SWAMPERT},
+    {name: "Troy", bal: 1500, locnum: 0, injail: 0, num: 3, color: "#FFF", spriteId: Sprite.VENUSAUR},
+    {name: "Poop", bal: 1500, locnum: 0, injail: 0, num: 4, color: "#000", spriteId: Sprite.CHARIZARD}
+];
+
+// Prefer images to text for special locations.
 /*var locText=[" ","","CC","","Tax"," ","","?","","",
             "Just Visiting","","","",""," ","","CC","","",
             "$ 0","","?","",""," ","","","","",
             "To Jail","","","CC",""," ","?",""," "];*/
 
-            
-for(var i=0;i<=39;i++){
-    var newdiv=document.createElement("div");
-    newdiv.dataset.no=i;
-    var indiv=document.createElement("div");
-    newdiv.style.backgroundColor=places[i].col;
-    if(places[i].ho){indiv.style.backgroundColor="rgb(213,232,212)"}//no text->indiv neutral
-    
-    switch(Math.floor(i/10)){
-        case 0:
-            newdiv.className="location bottom";
-            newdiv.style.right=70*i+"px";
-            indiv.style.bottom=0;
-            indiv.className="horizontal";
-            break;
-        case 1:
-            newdiv.className="location left";
-            newdiv.style.top=680-68*(i-10)+"px";
-            indiv.style.left=0;
-            indiv.className="vertical";
-            break;
-        case 2:
-            newdiv.className="location top";
-            newdiv.style.right=700-70*(i-20)+"px";
-            indiv.style.top=0;
-            indiv.className="horizontal";
-            break;
-        case 3:
-            newdiv.className="location right";
-            newdiv.style.top=68*(i-30)+"px";
-            indiv.style.right=0;
-            indiv.className="vertical";
-            break;
+function buildGameBoard() {
+    // Build and place all board locations on DOM.
+
+    const board = document.getElementById("board");
+
+    for (let i = 0; i <= 39; i ++){
+        const newdiv = document.createElement("div");
+        newdiv.dataset.no = i;
+        const indiv = document.createElement("div");
+        newdiv.style.backgroundColor = places[i].col;
+        if (places[i].ho) { // Color walkway neutral for house-able properties.
+            indiv.style.backgroundColor = "rgb(213,232,212)";
+        }
+        
+        switch (Math.floor(i / 10)) {
+            case 0:
+                newdiv.className = "location bottom";
+                newdiv.style.right = 70 * i + "px";
+                indiv.style.bottom = 0;
+                indiv.className = "horizontal";
+                break;
+            case 1:
+                newdiv.className = "location left";
+                newdiv.style.top = 680 - 68 * (i - 10) + "px";
+                indiv.style.left = 0;
+                indiv.className = "vertical";
+                break;
+            case 2:
+                newdiv.className = "location top";
+                newdiv.style.right = 700 - 70 * (i - 20) + "px";
+                indiv.style.top = 0;
+                indiv.className = "horizontal";
+                break;
+            case 3:
+                newdiv.className = "location right";
+                newdiv.style.top = 68 * (i - 30) + "px";
+                indiv.style.right = 0;
+                indiv.className = "vertical";
+                break;
+        }
+        newdiv.appendChild(indiv);
+        board.appendChild(newdiv);
     }
-    newdiv.appendChild(indiv);
-    board.appendChild(newdiv);
+
+    // Free Parking: add #alltax
+    board.childNodes[20].firstChild.id = "alltax";
+
+    // Go: set image
+    board.childNodes[0].style.background = "url('go.svg') no-repeat";
+
+    // Railroads: set images
+    for(let i = 5; i <= 35; i += 10) {
+        const railroad = board.childNodes[i];
+        railroad.style.background = "url('rr.svg') no-repeat";
+        railroad.style.backgroundSize = "68px 66px";
+    }
+
+    $(".location").click(function() {
+        showCard(this.dataset.no);
+    });
 }
 
-board.childNodes[20].firstChild.id="alltax";board.childNodes[20].firstChild.id="alltax";
+function buildPlayerViews() {
+    // Build each player view.
 
-board.childNodes[0].style.background="url('go.svg') no-repeat";
+    const board = document.getElementById("board");
 
-for(var loop1=5;loop1<=35;loop1+=10){
-    board.childNodes[loop1].style.background="url('rr.svg') no-repeat";
-    board.childNodes[loop1].style.backgroundSize="68px 66px";
+    players.forEach((player, i) => {
+        const circ = document.createElement("img");    
+        circ.id = "marker" + i;
+        circ.className = "circ";
+        circ.src = "http://veekun.com/dex/media/pokemon/dream-world/" + player.spriteId + ".svg";
+        board.childNodes[0].appendChild(circ);
+
+        const heads = document.getElementById("heads");
+        heads.innerHTML+="<div id='head" + i + "' class='head'></div><div style='background-color:" + player.color + ";height:5px'></div>";
+        heads.innerHTML+="<div class='dashboard' id='user" + i + "' style='display:none'><span id='ploc" + i + "'></span></div>";
+    });
+}
+
+function buildPlayerDisplays() {
+    // Set up the HUD for each player: name, location, and balance.
+    players.forEach((player, i) => {
+        $("#head" + i).html(player.name+": <span id='loc" + i + "'>Go</span><div style='float:right' id='bal" + i + "'>$1500</div>");    
+
+        // Expand HUD on click.
+        $("#head" + i).click(function() {
+            if (document.getElementById("user" + i).style.display == "none") {
+                slide(i);
+            }
+        });
+    });
+}
+
+function slide(user) {
+    // Collapse HUDs for all but current user.
+    players.forEach((player, i) => {
+        if (i != user) {
+            $("#user" + i).slideUp();
+        } else {
+            $("#user" + i).slideDown();
+        }
+    });
 }
 
 
-var playerCol=["#E80","#0C0","#08F","#FFF","#000"];
-var imgs=[257,254,260,3,6];
-for(var j=0;j<len;j++){
-    var circ=document.createElement("img");    
-    circ.id="marker"+j;
-    circ.className="circ";
-    circ.src="http://veekun.com/dex/media/pokemon/dream-world/"+imgs[j]+".svg";
-    //circ.style.backgroundColor=playerCol[j];
-    board.childNodes[0].appendChild(circ);
+var n = Math.ceil(Math.random() * players.length) - 1;
+var tax=0;
 
-    heads.innerHTML+="<div id='head"+j+"' class='head'></div><div style='background-color:"+playerCol[j]+";height:5px'></div>";
-    heads.innerHTML+="<div class='dashboard' id='user"+j+"' style='display:none'><span id='ploc"+j+"'></span></div>";
+buildGameBoard();
+buildPlayerViews();
+buildPlayerDisplays();
 
-
-
-
-}
-
-
-
-
-$("#turn").text(movers[n].name);
-$("#loc").text(places[movers[n].locnum].name);
-$(".location").click(function() {showCard(this.dataset.no)});
-
-});
+$("#turn").text(players[n].name);
+$("#loc").text(places[players[n].locnum].name);
