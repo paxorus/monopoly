ed9d88ae1a107f9353e1ced622c9f005663c4a5f
Prakhar Sahay <prakhar.studios@gmail.com>
2022-06-05 19:23:45 -0400

    [Execute Turn] DRY Tests

--- server/test/game-logic/execute-turn-properties.test.js
+++ server/test/game-logic/execute-turn-properties.test.js
@@ -1,7 +1,6 @@
 const assert = require("assert");
 const proxyquire = require("proxyquire");
-const {Game, GameRecord} = require("../../models/game.js");
-const {PlayerRecord} = require("../../models/player.js");
+const {getFreshGame} = require("../test-utils/execute-turn-utils.js");
 const RandomInt = require("../../fickle/random-int.js");
 
 
@@ -19,43 +18,6 @@ describe("Execute Turn: Properties", () => {
 		// Mock out obeyLocation, which is tested separately.
 		"./obey-location.js": { obeyLocation() { obeyLocationCalls ++; }}
 	});
-
-	class MockSocket {
-		constructor() {
-			this.messages = [];
-		}
-
-		emit(message, data) {
-			this.messages.push([message, data]);
-		}
-
-		resetMock() {
-			this.messages = [];
-		}
-	}
-
-	function getFreshGame() {
-		const game = new Game(new GameRecord("my game id", "my game name", "my admin id", [
-			new PlayerRecord("player 0 name", "user id 0", 0, "player sprite 0"),
-			new PlayerRecord("player 1 name", "user id 1", 1, "player sprite 1"),
-			new PlayerRecord("player 2 name", "user id 2", 2, "player sprite 2")
-		], [
-			{placeIdx: 37, ownerNum: 0, houseCount: 0, isMortgaged: true},
-			{placeIdx: 39, ownerNum: 0, houseCount: 2, isMortgaged: false}
-		]));
-
-		game.currentPlayerId = 0;
-
-		const player0 = game.players[0];
-		const mockSocket0 = new MockSocket();
-		player0.configureEmitter(null, mockSocket0);
-
-		return {
-			game,
-			player0,
-			mockSocket0
-		};
-	}
 
 	describe("#respondToBuyOffer()", () => {
 		it("buys the property for the player", () => {
--- server/test/game-logic/execute-turn-rolls.test.js
+++ server/test/game-logic/execute-turn-rolls.test.js
@@ -2,8 +2,7 @@ const assert = require("assert");
 const proxyquire = require("proxyquire");
 const RandomInt = require("../../fickle/random-int.js");
 const TimeNow = require("../../fickle/time-now.js");
-const {Game, GameRecord} = require("../../models/game.js");
-const {PlayerRecord} = require("../../models/player.js");
+const {getFreshGame, MockSocket} = require("../test-utils/execute-turn-utils.js");
 
 
 describe("Execute Turn: Rolls", () => {
@@ -18,43 +17,6 @@ describe("Execute Turn: Rolls", () => {
 		// Mock out obeyLocation, which is tested separately.
 		"./obey-location.js": { obeyLocation() { obeyLocationCalls ++; }}
 	});
-
-	class MockSocket {
-		constructor() {
-			this.messages = [];
-		}
-
-		emit(message, data) {
-			this.messages.push([message, data]);
-		}
-
-		resetMock() {
-			this.messages = [];
-		}
-	}
-
-	function getFreshGame() {
-		const game = new Game(new GameRecord("my game id", "my game name", "my admin id", [
-			new PlayerRecord("player 0 name", "user id 0", 0, "player sprite 0"),
-			new PlayerRecord("player 1 name", "user id 1", 1, "player sprite 1"),
-			new PlayerRecord("player 2 name", "user id 2", 2, "player sprite 2")
-		], [
-			{placeIdx: 37, ownerNum: 0, houseCount: 0, isMortgaged: true},
-			{placeIdx: 39, ownerNum: 0, houseCount: 2, isMortgaged: false}
-		]));
-
-		game.currentPlayerId = 0;
-
-		const player0 = game.players[0];
-		const mockSocket0 = new MockSocket();
-		player0.configureEmitter(null, mockSocket0);
-
-		return {
-			game,
-			player0,
-			mockSocket0
-		};
-	}
 
 	describe("#concludeTurn()", () => {
 		it("emits the correct message", () => {
